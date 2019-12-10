c2e4790f7114e3369ca9d0684955cd873e811de4
Thomas Uphill <thomas@narrabilis.com>
2019-12-10 14:27:43 -0800

    initial

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+# @summary This is a sample to include in a Puppetfile with a :git location
+#
+# This class does nothing
+#
+# @example
+#   include sample
+class sample {
+  notify {"Sample class": }
+}
